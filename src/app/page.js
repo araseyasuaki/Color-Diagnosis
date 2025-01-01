2022880521e67e573f13ab31fdc21666faaf14b7
arase <23aw0103@jec.ac.jp>
2025-01-01 18:25:22 +0900

    最初の待機画面のスタイル作成

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,15 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className=''>
-      <h1>色で見る性格診断</h1>
-      <p>8つの色からあなたの性格を診断します！</p>
-      <Link href="/question1">
-        <button>
-          診断を始める
-        </button>
+    <div className='flex flex-col justify-center items-center h-screen relative'>
+      <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-orange-500 via-yellow-500 via-green-500 via-blue-500 via-purple-500 via-pink-500 to-cyan-400">
+        展示会で使う色の診断です!!
+      </h1>
+      <p className='text-xl font-bold mt-5'>基本的に全5問...</p>
+      <Link href="/question">
+      <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 w-60 font-bold text-2xl text-black bg-white py-5 px-8 border-black border-2 rounded-full hover:text-white hover:bg-gradient-to-r hover:from-red-500 hover:via-orange-500 hover:via-yellow-500 hover:via-green-500 hover:via-blue-500 hover:via-purple-500 hover:via-pink-500 hover:to-cyan-400 transition-all text-center">
+        診断を始める
+      </button>
       </Link>
     </div>
   );
